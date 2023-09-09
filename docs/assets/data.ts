5eab096dfd952500fdba0078f9606426ea5437f1
SkCodeRains <68813335+SkCodeRains@users.noreply.github.com>
2023-09-09 15:53:20 +0530

    latest build updated

--- docs/assets/data.ts
+++ docs/assets/data.ts
@@ -256,6 +256,382 @@ export const Advanced_Internet_Programming: PeriodicElement[] = [
 ]
 
 
+export const System_Programming_And_Operating_System: PeriodicElement[] = [
+    {
+        Q: "Which of the following are Linux operating system? ",
+        A: "buntu ,Fedora, Redhat"
+    }, {
+        Q: "In contiguous memory allocation_______.",
+        A: "Each process is contained ina single contiguous section of memory"
+    }, {
+        Q: "Multiprogramming systems'",
+        A: "Execute more jobs in the same time period"
+    }, {
+        Q: "If one site fails in distributed system",
+        A: "remaining sites will continue their working"
+    }, {
+        Q: "Which one of the following is not task of an operating system?",
+        A: "CPU On and off"
+    }, {
+        Q: "Which of the following is the feature of multiprocessing systems?",
+        A: "Increased throughput"
+    }, {
+        Q: "From the following, which is not a common file permission ?",
+        A: "Stop"
+    }, {
+        Q: "The main function of a Command Interpreter is __________.",
+        A: "to get and execute the next user-specified command"
+    }, {
+        Q: "Which of the following are used in command interpreter?",
+        A: "Routines and system calls"
+    }, {
+        Q: "Which of the following are attributes of a file?",
+        A: "name , size , location , bitraite , read , write , access"
+    }, {
+        Q: "What are the different types of distributed systems?",
+        A: "Peer to Peer model and client server model"
+    }, {
+        Q: "A _____ sytem Guarantees that critical tasks complete on time.",
+        A: "Hard real time"
+    }, {
+        Q: "For every process there is a______.",
+        A: "Page table"
+    }, {
+        Q: "____are very similar to Multiprogramming batch system?",
+        A: "Time sharing system"
+    }, {
+        Q: "We want to keep CPU as busy as possible This criteria refers to be.",
+        A: "CPU utilization"
+    }, {
+        Q: "Which of the following is an example of CUI?",
+        A: "DOS"
+    }, {
+        Q: "Which of the following comes under the category of external security?",
+        A: "Floods"
+    }, {
+        Q: "A pool of jobs on disk allows the OS to select which job to run next to _____CPU utilization?",
+        A: "Increase"
+    }, {
+        Q: "Which of the following is not an  example of an operating system",
+        A: "Adobe"
+    }, {
+        Q: "Which of the following is not an  example of an operating system?",
+        A: "MS Word"
+    }, {
+        Q: "_______Translate hish level language to machine language?",
+        A: "Linker"
+    }, {
+        Q: "What is scheduling?",
+        A: "Allowing a job to use the processor"
+    }, {
+        Q: "_______Provides The means of proper use of hardware in the operations of the computer system ,it is similar to government?",
+        A: "Operating system"
+    }, {
+        Q: "If a process fails, most operating system write the error information to a",
+        A: "Log life"
+    }, {
+
+        Q: `Which of the following is a strong password ?
+
+        a.
+        .!augustdelhi
+        
+        b.
+        123pa$sworD
+        
+        c.
+        19thAugust88
+        
+        d.
+        Delhi88`,
+        A: "123pa$sworD"
+    }, {
+        Q: "Linus is an _____Operating system?",
+        A: "Open source"
+    }, {
+        Q: "What are the different types of multiprocessing systems?",
+        A: "Symmeteric and Asymmeteric"
+    }, {
+        Q: "________Is process in which communication between processes takes place by calls send and receive prmitives?",
+        A: "Sychronization"
+    }, {
+        Q: "Expand GUI. ",
+        A: "Graphical user interface"
+    }, {
+        Q: "The _____model maps each user threads to kernel thread.",
+        A: "One-to-one"
+    }, {
+        Q: `Most operating systems are comprised of three main components: the ........................., the file system, and the shell.
+
+        a.
+        Kernel
+        
+        b.
+        User inetrface
+        
+        c.
+        All of these
+        
+        d.
+        Desktop`,
+        A: "Kernel"
+    }, {
+        Q: "The strategy of making processes that are logically unable to be temporily suspended is called___?",
+        A: "Preemptive scheduling"
+    }, {
+        Q: "Which comes firsts in protection of an operating system? ",
+        A: "Authorization"
+    }, {
+        Q: "_________is Performed during the compilation of source program?",
+        A: "Static linking"
+    }, {
+        Q: "Most operating systems are comprised of three main components: the ........................., the file system, and the shell.",
+        A: "Kernel"
+    }, {
+        Q: "Which is not the state of the process?",
+        A: "Privilege"
+    }, {
+        Q: "A Microsoft Windows is a(n)....................................",
+        A: "Operating system"
+    }, {
+        Q: "Which of the following were used in 1st generation ?",
+        A: "Vacuum tubes"
+    }, {
+        Q: "Which of the following is real time operating system.",
+        A: "Process control"
+    }, {
+        Q: "Each process has a code segment, called______.",
+        A: "Critical section"
+    }, {
+        Q: "Which of  the following components are shared by distributed operating system?",
+        A: "Both clock and memory"
+    }, {
+        Q: "In ______ OS, the response time is very critical. ",
+        A: "Real time"
+    }, {
+        Q: "Virtual memory is normally implemented by_______.",
+        A: "Demand paging"
+    }, {
+        Q: "Cryptography technique is used in ________. ",
+        A: "Protection"
+    }, {
+        Q: "A mutex is also a common name for a program object that negotiates mutual exclusion among threads also called a_____.",
+        A: "Lock"
+    }, {
+        Q: "Which of the following is a good practice ?",
+        A: "Grant limited permission to specified account"
+    }, {
+        Q: "Cryptography technique is used in ________. ",
+        A: "Protection"
+    }, {
+        Q: "in______Communication each process that want to communicate must explicitly name the recipient or sender of the communication.",
+        A: "Direct"
+    }, {
+        Q: "_______Is used to manage the computer hardware and application software and performs small tasks?",
+        A: "Utility software"
+    }, {
+        Q: `Which one of the following error will be handle by the operating system?
+
+        a.
+        lack of paper in printer
+        
+        b.
+        .connection failure in the network
+        
+        c.
+        All of these
+        
+        d.
+        Power failure`,
+        A: "All of these"
+    }, {
+        Q: "The primary purpose of an operating system is'",
+        A: "To make the most efficient use of the computer hardware"
+    }, {
+        Q: "Hard real time operating system has ___ jitter than a soft real time operating system.",
+        A: "less"
+    }, {
+        Q: "Which of the following is not an  example of an operating system?",
+        A: "MS Word"
+    }, {
+        Q: "The most optimal scheduling algorithm is :",
+        A: "SJF – Shortest Job First"
+    }, {
+        Q: "Multithreading can be greatly increased in a_____Architecture?",
+        A: "Multiprocessor"
+    }, {
+        Q: "Master and slave relationship is used in which of the following system?",
+        A: "Multiprocessing system."
+    }, {
+        Q: "Which of the following algorithm tends to minimize the process of flow time?",
+        A: "Shortest job first"
+    }, {
+        Q: `Process is
+
+        a.
+        contents of main memory
+        
+        b.
+        Program in High level language kept on disk
+        
+        c.
+        All of these
+        
+        d.
+        .. a program in execution`,
+        A: "a program in execution"
+    }, {
+        Q: `A process can be terminated due to
+
+        a.
+        killed by another process
+        
+        b.
+        All of these
+        
+        c.
+        Normal exits
+        
+        d.
+        fatal error`,
+        A: "All"
+    }, {
+        Q: `________places the instruction and data into memory?`,
+        A: "Loading"
+    }, {
+        Q: `The FCFS algorithm is particularly troublesome for`,
+        A: "Multiprogramming"
+    }, {
+        Q: `Expand DOS. `,
+        A: "Disk operating system"
+    }, {
+        Q: `_____ Contain a copy of the content of the memory location stored in MAR?`,
+        A: "MBR"
+    }, {
+        Q: `Which of the following is the function of memory management component?`,
+        A: "Allocation and deallocation of memory"
+    }, {
+        Q: `Microsoft windows is______?`,
+        A: "Operating system"
+    }, {
+        Q: `_______Is synchrnization tool that does not require busy Awaiting?`,
+        A: "Semaphores"
+    }, {
+        Q: `In fixed size partition the degree of multiprogramming is bounded by ________.`,
+        A: "The number of partitions"
+    }, {
+        Q: `Who is called supervisor of computer activity?`,
+        A: "Operating system"
+    }, {
+        Q: `What is operating system?`,
+        A: "Acollection of programs that manages hardware resources"
+    }, {
+        Q: `A process can be terminated due to
+
+        a.
+        killed by another process
+        
+        b.
+        All of the options are correct
+        
+        c.
+        normal exit
+        
+        d.
+        fatal error`,
+        A: "All of the options are correct"
+    }, {
+        Q: `_______Is the function responsible for managing the computer's primary memory?
+
+        a.
+        Memory management
+        
+        b.
+        None of these
+        
+        c.
+        Low memory
+        
+        d.
+        High memory`,
+        A: "Memory management"
+    }, {
+        Q: `Which scheduling algorithm allocates the CPU first to the process that requests the CPU first?
+
+        a.
+        Priority scheduling
+        
+        b.
+        Fisrt come first served scheduling
+        
+        c.
+        none of the mentioned
+        
+        d.
+        Shortest job scheduling`,
+        A: "Shortest job scheduling"
+    }, {
+        Q: `Expand CUI
+
+        a.
+        None of these
+        
+        b.
+        command user interface
+        
+        c.
+        Control user interface
+        
+        d.
+        Correct user interface`,
+        A: " command user interface"
+    }, {
+        Q: `In a multiprogramming systems, CPU is 
+
+        a.
+        Does jobs one by one.
+        
+        b.
+        Always free
+        
+        c.
+        Does one process in a day
+        
+        d.
+        Never idle.`,
+        A: "Never idle."
+    }, {
+        Q: `From the following, which is a common file permission ?
+
+        a.
+        Write
+        
+        b.
+        Execute
+        
+        c.
+        All of these
+        
+        d.
+        Read`,
+        A: "All of these"
+    }, {
+        Q: `_____Threads are supported directly by the operating system?
+
+        a.
+        Shell
+        
+        b.
+        Kernel
+        
+        c.
+        Os
+        
+        d.
+        Multi`,
+        A: ""
+    }
+]
 export const applicationData = {
     MCA: [
         {
@@ -264,7 +640,11 @@ export const applicationData = {
                 {
                     SUBJECT_NAME: "Advanced Internet Programming",
                     DATA: Advanced_Internet_Programming
-                }
+                },
+                {
+                    SUBJECT_NAME: "System Programming & Operating System",
+                    DATA: System_Programming_And_Operating_System
+                },
             ],
         }
     ]
